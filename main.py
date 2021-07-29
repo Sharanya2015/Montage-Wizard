23db066887721b5374181f0dae326f16dbdcd0f4
Sharanya Banerjee <48113931+Sharanya2015@users.noreply.github.com>
2021-07-29 16:47:33 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,79 +1,85 @@
-import time
-import cv2 as cv2
-import easyocr
+from flask import Flask, render_template, request, jsonify, json
+import requests
 from datetime import datetime
-from PIL import Image
-import os, re, os.path
-import sys
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
-reader = easyocr.Reader(['ch_sim','en'])
+app = Flask(__name__)
 
-current_frame = 0
-frame_check_rate = 60
-start_minus = 15
-end_plus = 15
-result_list = []
-left = 600
-top = 600
-right = 1200
-bottom = 900
-is_clip = False
-start_time = 0
-latest_knockdown_time = 0
-
-#scan the download directory for video file
-filename = "shiv.mkv"
-cam = cv2.VideoCapture(filename)
-
-#read the video file
-while(True):
-	ret, frame = cam.read()
-	if ret:
-		print("read a frame")
-		name = str(current_frame) + '.jpg'
-		if(current_frame%frame_check_rate == 0):
-			print("current frame is nth frame")
-			status = cv2.imwrite(name,frame)
-			im = Image.open(name)
-			im1 = im.crop((left, top , right, bottom))
-			im1 = im1.save(name)
-			result = reader.readtext(name, detail =0)
-			curr_time = cam.get(cv2.CAP_PROP_POS_MSEC)/1000
-			result_list.append(result) #I dont think we need this anymore
-			
-			os.remove(name)  #delete the saved file 
-			
-			for res in result:
-				if "KNOCK" in res or "KOCK" in res or "NATED" in res or "ELMI" in res or "ASS" in res or "IST" in res or "KNU" in res or "DOwN" in res or "ELM" in res or "EUM" in res:
-					print('knocked!!!' + str(curr_time))
-					latest_knockdown_time = curr_time
-					if(is_clip == False):
-						print("is clip is false")
-						start_time = curr_time
-						is_clip = True
-			if(is_clip):
-				if((curr_time - latest_knockdown_time) >= 25):
-					end_time = latest_knockdown_time + end_plus
-					start_time = start_time - start_minus
-					print("making a clip from" + str(start_time) + "_" + str(end_time))
-					#TODO: change it later
-					clipname = "Knock_" +str(start_time) + "_" + str(end_time) +".mp4"
-					ffmpeg_extract_subclip(filename, start_time, end_time, targetname = clipname) 
-					is_clip = False
-		current_frame += 1
-	else:
-		print("Good Job, reading all the frames is complete")
-
-		if(is_clip):
-			end_time = latest_knockdown_time + end_plus
-			start_time = start_time -start_minus
-			print("Generating one more clip")
-			filename = "Knock_" + str(start_time) +"_"+ str(end_time) + ".mp4"
-			ffmpeg_extract_subclip(filename,start_time,end_time,targetname=clipname)
-			break
-		break
+@app.route("/", methods=['GET', 'POST'])
+def homepage():
+    return render_template("index.html", title="Twitch Streamer VODs")
 
 
-						
+@app.route("/LoadVodList", methods=['GET', 'POST'])
+def LoadVodList():
 
+    #########################
+    ## GETTING VIDEOS LIST ##
+    #########################
+
+    ## Its the name you see when you browse to the twitch url of the streamer
+    USER_ID = request.args.get('input')
+
+    CLIENT_ID = "ieshhz3qdq2c8iwnibrw9988d3trey" 
+    SECRET = "drvli9opyl07t7i8o5drocdk8okhd2"
+
+    ## First get a local access token. 
+    secretKeyURL = "https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&grant_type=client_credentials".format(CLIENT_ID, SECRET)
+    responseA = requests.post(secretKeyURL)
+    accessTokenData = responseA.json()
+
+    ## Then figure out the user id. 
+    userIDURL = "https://api.twitch.tv/helix/users?login=%s"%USER_ID
+    responseB = requests.get(userIDURL, headers={"Client-ID":CLIENT_ID,
+                                                    'Authorization': "Bearer "+accessTokenData["access_token"]})
+    userID = responseB.json()["data"][0]["id"]
+
+
+    ## Now you can request the video data.
+    findVideoURL = "https://api.twitch.tv/helix/videos?user_id=%s"%userID
+    responseC= requests.get(findVideoURL, headers={"Client-ID":CLIENT_ID,
+                                                    'Authorization': "Bearer "+accessTokenData["access_token"]})
+    print(responseC.json())
+    data_array = []
+    data_array.append('VIDEOS')
+    for video in responseC.json()["data"]:
+        item = {}
+        item['Title'] = video['title']
+        item['Date'] = video['created_at'].split("T")[0]
+        item['Duration'] = video['duration']
+        item['Thumbnail_url'] = video['thumbnail_url'].replace('%','').format(width = "192", height = "108")
+        item['vod_id'] = video['id']
+
+        if len(data_array) < 11:
+            data_array.append(item)
+    
+    #####################################
+    ## VIDEOS RETRIEVED, GETTING CLIPS ##
+    #####################################
+
+    findVideoURL2 = "https://api.twitch.tv/helix/clips?broadcaster_id=%s"%userID
+    responseD= requests.get(findVideoURL2, headers={"Client-ID":CLIENT_ID,
+                                                    'Authorization': "Bearer "+accessTokenData["access_token"]})
+
+    sorted_clips = sorted(responseD.json()['data'], key=lambda x: datetime.strptime(x['created_at'], '%Y-%m-%dT%H:%M:%SZ'), reverse=True)
+    print(sorted_clips)
+
+    data_array.append('CLIPS')
+    for video in sorted_clips:
+        item = {}
+        item['Title'] = video['title']
+        item['Date'] = video['created_at'].split("T")[0]
+        item['Duration'] = str(video['duration']) + 's'
+        item['Thumbnail_url'] = video['thumbnail_url'].replace('%','').format(width = "192", height = "108")
+        item['clip_id'] = video['id']
+
+        if len(data_array) < 22:
+            data_array.append(item)
+    
+    return_json = json.dumps(data_array)
+    return (jsonify({'response':return_json}))
+
+def sortFunction(value):
+    return value['created_at']
+
+if __name__ == "__main__":
+    app.run(debug=True)
